f7cf6a394da573f323981000551d14cfb83c5f39
waldls <p353700@gmail.com>
2026-01-20 07:38:09 +0000

    chore: 홈화면 첫 번째 사진 수정

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,19 @@
+import Error404 from '@/assets/images/error/Error404.svg?react';
+import SecondaryButton from '@/components/Button/SecondaryButton'
+import { useNavigate } from 'react-router-dom';
+
 const NotFoundPage = () => {
-  return <div>404 Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="min-h-[calc(100vh-80px)] flex items-center justify-center">
+      <div className="flex flex-col gap-32 items-center">
+        <p className="font-heading-3 text-gray-400 text-center">Page Disconnected</p>
+        <Error404 />
+        <SecondaryButton className="w-280" text="홈으로 가기" onClick={() => navigate('/')} />
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
