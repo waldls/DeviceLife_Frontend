a30366c69c5956e014819dab4bd97a95b0b42756
waldls <p353700@gmail.com>
2026-01-27 03:00:58 +0000

    fix: 푸터 외부링크 클릭 시 새로운 창 열리도록 수정

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -5,46 +5,55 @@ const Footer = () => {
         <div className="flex flex-wrap items-start gap-96">
           <a
             href="https://lovely-potassium-7f2.notion.site/2f0c82f125c9807a8fdfdcdcaa204c5a?pvs=74"
+            target="_blank"
+            rel="noopener noreferrer"
             className="w-140 text-white"
           >
             <span className="font-body-1-sm link-underline">공지사항</span>
           </a>
           <a
             href="https://lovely-potassium-7f2.notion.site/2f0c82f125c980a5be42d20615f296b1?pvs=74"
+            target="_blank"
+            rel="noopener noreferrer"
             className="w-140 text-white"
           >
             <span className="font-body-1-sm link-underline">자주묻는질문</span>
           </a>
           <a
             href="https://lovely-potassium-7f2.notion.site/2f0c82f125c980949ebbf75f5e0797f7"
+            target="_blank"
+            rel="noopener noreferrer"
             className="w-180 text-white"
           >
             <span className="font-body-1-sm link-underline">개인정보처리방침</span>
           </a>
           <a
             href="https://lovely-potassium-7f2.notion.site/2f0c82f125c980bea108c01efb702ef7?pvs=74"
+            target="_blank"
+            rel="noopener noreferrer"
             className="w-140 text-white"
           >
             <span className="font-body-1-sm link-underline">이용약관</span>
           </a>
           <a
             href="https://lovely-potassium-7f2.notion.site/2f0c82f125c980fa8fa0d2ef430bbe79?pvs=74"
+            target="_blank"
+            rel="noopener noreferrer"
             className="w-140 text-white"
           >
             <span className="font-body-1-sm link-underline">고객센터</span>
           </a>
         </div>
         <div className="flex flex-col gap-12 items-start w-fit">
-          <p className="font-body-1-sm text-white">(주) Device Life</p>
+          <p className="font-body-1-sm text-white">Team Device Life</p>
           <p className="font-body-3-r text-white">
-            대표: 김준환 | 사업자등록번호: 105-30-20492 | 통신판매업: 2023-서울마포-2465
+            UMC 9th (University MakesUs Challenge) | 2025.09 ~ 2026.02
           </p>
-          <p className="font-body-3-r text-white">
-            이메일: ssy08042@kau.kr | 주소: 서울특별시 마포구 와우산로 94, 7층
-          </p>
+          <p className="font-body-3-r text-white">Contact: ssy08042@kau.kr</p>
         </div>
+
         <p className="self-stretch font-body-3-r text-white">
-          © Device Life Co, All rights reserved.
+          © 2026 Device Life, All rights reserved.
         </p>
       </div>
     </footer>
